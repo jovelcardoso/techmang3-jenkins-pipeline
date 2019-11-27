725964bde33df18e2e30f4c03672c2d15c6e5dfb
jovel-uc <jovelcc@unicourt.com>
2019-11-27 16:55:51 +0530

    test

--- vars/analytics_test.groovy
+++ vars/analytics_test.groovy
@@ -1,18 +1,15 @@
-def call() {
+def call(gitUsername, repositoryName) {
 
     stage('checkout') {
         node('master') {
             // Clear all the files and directory for the clean build
             sh "rm -rf *";
             // Cloning the repostiroy which stores the data to build the docker file and the kube deployment files
-//            sh "git clone --single-branch -b master https://github.com/${gitUsername}/${repositoryName}.git";
+            sh "git clone --single-branch -b master https://github.com/${gitUsername}/${repositoryName}.git";
             // Pulling the changes to the renderer repository.
 //            sh "cd ~/kube_renderer && git pull";
-            sh "pwd"
-            sh "cd ../../../"
-            sh "pwd"
 
-            sh "cd ~/home/mis/"
+//            sh "cd ~/home/mis/"
 
             sh "cd insight/"
             sh "ls"
