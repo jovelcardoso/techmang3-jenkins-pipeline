456bdf4fd889f0ac45cadd434aa3d838c7c21a63
jovel-uc <jovelcc@unicourt.com>
2019-11-28 16:38:43 +0530

    test

--- vars/parallel_test.groovy
+++ vars/parallel_test.groovy
@@ -22,33 +22,32 @@ def call(gitUsername, repositoryName) {
 //        }
 //    }
 
-    stages {
-        stage('Run Tests') {
-            parallel {
-                stage('Test On Windows') {
-                    agent {
-                        label "windows"
-                    }
-                    steps {
-                        bat "run-tests.bat"
-                    }
-                    post {
-                        always {
-                            junit "**/TEST-*.xml"
-                        }
+
+    stage('Run Tests') {
+        parallel {
+            stage('Test On Windows') {
+                agent {
+                    label "windows"
+                }
+                steps {
+                    bat "run-tests.bat"
+                }
+                post {
+                    always {
+                        junit "**/TEST-*.xml"
                     }
                 }
-                stage('Test On Linux') {
-                    agent {
-                        label "linux"
-                    }
-                    steps {
-                        sh "run-tests.sh"
-                    }
-                    post {
-                        always {
-                            junit "**/TEST-*.xml"
-                        }
+            }
+            stage('Test On Linux') {
+                agent {
+                    label "linux"
+                }
+                steps {
+                    sh "run-tests.sh"
+                }
+                post {
+                    always {
+                        junit "**/TEST-*.xml"
                     }
                 }
             }
