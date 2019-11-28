d62d10171ded792e58f4c860d81ff72210915b7e
jovel-uc <jovelcc@unicourt.com>
2019-11-28 16:35:00 +0530

    test

--- vars/parallel_test.groovy
+++ vars/parallel_test.groovy
@@ -1,4 +1,39 @@
-def call() {
+def call(gitUsername, repositoryName) {
+
+    stage('checkout') {
+        node('master') {
+            // Clear all the files and directory for the clean build
+            sh "rm -rf *";
+            // Cloning the repostiroy which stores the data to build the docker file and the kube deployment files
+            sh "git clone --single-branch -b master https://github.com/${gitUsername}/${repositoryName}.git";
+            // Pulling the changes to the renderer repository.
+//            sh "cd ~/kube_renderer && git pull";
+
+//            sh "cd ~/home/mis/"
+
+            sh "cd ${repositoryName}/"
+            sh "ls -sh"
+            // Setting up the minor build version
+            Date date = new Date()
+            // Populating the image version.
+            imageVersion = date.format("yyddMMHHmm");
+
+
+        }
+    }
+
+    stage('Build and Test') {
+        node('master') {
+            sh "ls -sh"
+            // Runing the test cases on the docker image if it is applicable.
+            stage('Test On Windows') {
+                sh "ls -sh"
+            }
+            stage('Test On Linux') {
+                sh "ls -sh"
+            }
+        }
+    }
     stages {
         stage('Run Tests') {
             parallel {
@@ -29,6 +64,23 @@ def call() {
                     }
                 }
             }
+        }
+    }
+
+    stage('Confirm') {
+        def userInput = input(
+                id: 'userInput', message: 'This is PRODUCTION!', parameters: [
+                [$class: 'BooleanParameterDefinition', defaultValue: false, description: '', name: 'Please confirm you sure to proceed']
+        ])
+
+        if(!userInput) {
+            error "Build was canceled";
+        }
+    }
+
+    stage('Deploy on Production') {
+        node('master') {
+            sh "echo 'Deploy on Production'";
         }
     }
 }
